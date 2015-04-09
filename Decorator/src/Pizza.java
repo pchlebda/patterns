cd83dcbd4d7a4a5f7c670aec6a67edc366b668be
pchlebda <piotrekchlebda@gmail.com>
2015-04-10 00:18:57 +0200

    Decorator design pattern

---
+++ Decorator/src/Pizza.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Piotr on 2015-04-10.
+ */
+public interface Pizza {
+
+    public String getDescription();
+
+    public double getCost();
+}
---
+++ Decorator/src/PlainPizza.java
@@ -0,0 +1,14 @@
+/**
+ * Created by Piotr on 2015-04-10.
+ */
+public class PlainPizza implements Pizza {
+    @Override
+    public String getDescription() {
+        return "Thin Dough";
+    }
+
+    @Override
+    public double getCost() {
+        return 4.00;
+    }
+}
