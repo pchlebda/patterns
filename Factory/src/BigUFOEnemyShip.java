aec63c344b318dd65b433b2e01dbd9fa5b7e3742
pchlebda <piotrekchlebda@gmail.com>
2015-05-03 20:32:04 +0200

    Factory design pattern

---
+++ Factory/src/BigUFOEnemyShip.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Piotr on 2015-05-03.
+ */
+public class BigUFOEnemyShip extends EnemyShip {
+
+    public BigUFOEnemyShip() {
+        setName("Big Enemy Ship");
+        setAmtDamage(40.0d);
+    }
+}
---
+++ Factory/src/UFOEnemyShip.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Piotr on 2015-05-03.
+ */
+public class UFOEnemyShip extends EnemyShip {
+
+    public UFOEnemyShip() {
+        setName("UFO Enemy Ship");
+        setAmtDamage(20.0d);
+    }
+}
